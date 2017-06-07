bb80cb80bc8117c3dfe77871ee7bbd877bbd3f4b
Yamie van Wijnbergen <Yamie@Yamies-MacBook-Pro.local>
2017-06-07 15:02:46 +0200

    viewcontrollers added

---
+++ YamievanWijnbergen-Programmeerproject/SearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SearchTableViewCell.swift
+//  YamievanWijnbergen-Programmeerproject
+//
+//  Created by Yamie van Wijnbergen on 07/06/2017.
+//  Copyright © 2017 Yamie van Wijnbergen. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
