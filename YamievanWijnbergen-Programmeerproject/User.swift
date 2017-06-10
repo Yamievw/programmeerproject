1b8df8c362cae802db6c998b46d39f1ae7dbf441
Yamie van Wijnbergen <Yamie@Yamies-MacBook-Pro.local>
2017-06-10 14:22:24 +0200

    Model added for user

---
+++ YamievanWijnbergen-Programmeerproject/User.swift
@@ -0,0 +1,18 @@
+//
+//  User.swift
+//  YamievanWijnbergen-Programmeerproject
+//
+//  Created by Yamie van Wijnbergen on 10/06/2017.
+//  Copyright © 2017 Yamie van Wijnbergen. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+    var name: String?
+    var email: String?
+    var profileImageUrl: String?
+    var experience: String?
+    var dives: String?
+    var certificate: String?
+}
